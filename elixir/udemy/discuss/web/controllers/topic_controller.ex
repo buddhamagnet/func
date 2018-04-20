f21c1b1fba1923736aea9295e63c4d17b1db8a7d
buddhamagnet <buddhamagnet@gmail.com>
2018-04-20 15:55:27 +0000

    correct model and start form build

--- elixir/udemy/discuss/web/controllers/topic_controller.ex
+++ elixir/udemy/discuss/web/controllers/topic_controller.ex
@@ -1,11 +1,17 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def index(conn, _params) do
     render conn, "index.html"
   end
 
-  def new(conn, _params) do
-    render conn, "new.html"
+  def new(conn, params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, params) do
   end
 end
