a6c4d7372c80da60330838e06d9218464f26d1a8
buddhamagnet <buddhamagnet@gmail.com>
2018-04-22 16:57:38 +0000

    more splurty

--- elixir/phoenix/splurty/lib/splurty_web/controllers/quote_controller.ex
+++ elixir/phoenix/splurty/lib/splurty_web/controllers/quote_controller.ex
@@ -1,8 +1,26 @@
 defmodule SplurtyWeb.QuoteController do
   use SplurtyWeb, :controller
 
+  alias SplurtyWeb.Quote
+  alias Splurty.Repo
+
   def index(conn, _params) do
-    quotes = Splurty.Repo.all(SplurtyWeb.Quote)
+    quotes = Repo.all(Quote)
     render conn, "index.html", quotes: quotes
+  end
+
+  def new(conn, params) do
+    changeset = Quote.changeset(%Quote{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+   def create(conn, %{"quote" => quote}) do
+    changeset = Quote.changeset(%Quote{}, quote)
+    case Repo.insert(changeset) do
+      {:ok, topic} ->
+        render conn, "index.html"
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
--- elixir/phoenix/splurty/lib/splurty_web/models/quote.ex
+++ elixir/phoenix/splurty/lib/splurty_web/models/quote.ex
@@ -5,4 +5,10 @@ defmodule SplurtyWeb.Quote do
     field :saying, :string
     field :author, :string
   end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:saying, :author])
+    |> validate_required([:saying, :author])
+  end
 end
