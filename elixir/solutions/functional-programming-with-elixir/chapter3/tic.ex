966a7fbad6822eef9ee76502abce270115ecb5b3
buddhamagnet <buddhamagnet@gmail.com>
2018-05-06 11:14:49 +0100

    more lix

--- elixir/solutions/functional-programming-with-elixir/chapter3/tic.ex
+++ elixir/solutions/functional-programming-with-elixir/chapter3/tic.ex
@@ -9,5 +9,13 @@ defmodule Tic do
   def winner({_, _, x, _, _, x, _, _, x}), do: {:winner, x}
   def winner({x, _, _, _, x, _, _, _, x}), do: {:winner, x}
   def winner({_, _, x, _, x, _, x, _, _}), do: {:winner, x}
+  def winner({y, y, y, _, _, _, _, _, _}), do: {:winner, y}
+  def winner({_, _, _, y, y, y, _, _, _}), do: {:winner, y}
+  def winner({_, _, _, _, _, _, y, y, y}), do: {:winner, y}
+  def winner({y, _, _, y, _, _, y, _, _}), do: {:winner, y}
+  def winner({_, y, _, _, y, _, _, y, _}), do: {:winner, y}
+  def winner({_, _, y, _, _, y, _, _, y}), do: {:winner, y}
+  def winner({y, _, _, _, y, _, _, _, y}), do: {:winner, y}
+  def winner({_, _, y, _, y, _, y, _, _}), do: {:winner, y}
   def winner(_board), do: :no_winner
 end
