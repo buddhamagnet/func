2518798ae39eb830650b2ad4dd1487216e77690e
buddhamagnet <buddhamagnet@gmail.com>
2018-05-08 13:45:59 +0100

    start build of components and youtube search

--- js/react/grider1/src/index.js
+++ js/react/grider1/src/index.js
@@ -1,1 +1,8 @@
-const App = () => <div>decimate</div>
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+const YTKey = 'AIzaSyB6iXh3MeXcQ8nzmj_K6WucDA6WPLXk998';
+
+const App = () => <div>Decimate</div>
+
+ReactDOM.render(<App />, document.querySelector('.container'));
