04462a01c4312a53876482bef4277bd74d1b8866
buddhamagnet <buddhamagnet@gmail.com>
2018-05-16 13:32:09 +0100

    building actions

--- js/react/grider_redux/src/containers/book_list.js
+++ js/react/grider_redux/src/containers/book_list.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import { selectBook } from '../actions';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+        onClick={() => this.props.selectBook(book)}
+        key={book.title}
+        className="list-group-item">
           {book.title}
+
         </li>
       );
     });
   }
   render() {
     return (
+      <div>
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
+      </div>
     )
   }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {selectBook}, dispatch
+  );
 }
 
 function mapStateToProps(state) {
@@ -26,4 +41,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
