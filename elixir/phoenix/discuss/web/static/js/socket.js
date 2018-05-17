be7524c0cf3301db742037bb180706b06b37cf33
buddhamagnet <buddhamagnet@gmail.com>
2018-05-17 16:31:36 +0100

    clean up js

--- elixir/phoenix/discuss/web/static/js/socket.js
+++ elixir/phoenix/discuss/web/static/js/socket.js
@@ -6,7 +6,7 @@ socket.connect()
 
 const createSocket = (topicID) => {
   const channelID = `comments:${ topicID }`;
-  const channel = socket.channel(channelID, {})
+  const channel = socket.channel(channelID, {});
   channel.join()
   .receive("ok", resp => {
     renderComments(resp.comments);
@@ -16,24 +16,28 @@ const createSocket = (topicID) => {
   channel.on(`comments:${ topicID }:new`, renderComment);
 
   document.querySelector('button').addEventListener('click', () => {
-    const content = document.querySelector('textarea').value;
+    const content = getElement('textarea').value;
     channel.push('comments:add', {content})
   });
 }
+
+const getElement = (name) => document.querySelector(name)
+
+const commentsList = () => getElement(".collection")
 
 const commentTemplate = (comment) => {
   return `<li class="collection-item">${comment.content}</li>`
 }
 
 const renderComment = (event) => {
-  document.querySelector(".collection").innerHTML += commentTemplate(event.comment);
+  commentsList().innerHTML += commentTemplate(event.comment);
 }
 
 const renderComments = (comments) => {
   const renderedComments = comments.map((comment) => {
     return commentTemplate(comment);
   });
-  document.querySelector(".collection").innerHTML = renderedComments.join('');
+  commentsList().innerHTML = renderedComments.join('');
 };
 
 
